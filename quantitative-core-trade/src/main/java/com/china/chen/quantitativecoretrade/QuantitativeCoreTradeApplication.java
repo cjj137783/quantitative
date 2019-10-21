f4821e187896e752f44ef8497f028e47957b92f9
chenjianjun <chenjianjun@huobi.com>
2019-10-21 18:28:12 +0800

    message: add huobi sdk

---
+++ quantitative-core-trade/src/main/java/com/china/chen/quantitativecoretrade/QuantitativeCoreTradeApplication.java
@@ -0,0 +1,13 @@
+package com.china.chen.quantitativecoretrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantitativeCoreTradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuantitativeCoreTradeApplication.class, args);
+    }
+
+}
---
+++ quantitative-core-trade/src/test/java/com/china/chen/quantitativecoretrade/QuantitativeCoreTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.china.chen.quantitativecoretrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantitativeCoreTradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
