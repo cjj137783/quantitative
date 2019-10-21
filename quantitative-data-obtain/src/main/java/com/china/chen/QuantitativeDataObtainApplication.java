bc46013f11cb2f3535b53bd63074ea6bf087e87b
chenjianjun <chenjianjun@huobi.com>
2019-10-21 15:07:16 +0800

    message:first commit

---
+++ quantitative-data-obtain/src/main/java/com/china/chen/QuantitativeDataObtainApplication.java
@@ -0,0 +1,13 @@
+package com.china.chen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantitativeDataObtainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantitativeDataObtainApplication.class, args);
+	}
+
+}
---
+++ quantitative-data-obtain/src/test/java/com/china/chen/QuantitativeDataObtainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.china.chen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantitativeDataObtainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
