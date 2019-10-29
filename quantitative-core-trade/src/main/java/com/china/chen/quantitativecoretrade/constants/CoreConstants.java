cc6fd06c76748deee15501e66ed5f76e227b1091
chenjianjun <chenjianjun@huobi.com>
2019-10-29 09:02:58 +0800

    message: add base function

--- quantitative-core-trade/src/main/java/com/china/chen/quantitativecoretrade/constants/CoreConstants.java
+++ quantitative-core-trade/src/main/java/com/china/chen/quantitativecoretrade/constants/CoreConstants.java
@@ -8,6 +8,6 @@ public class CoreConstants {
     /**交易币种的默认精度*/
     public static final Integer QUOTE_COIN_PRECISION = 6 ;
     /**钉钉报警的webhook*/
-    public static final String HOOK = "" ;
+    public static final String HOOK = "https://oapi.dingtalk.com/robot/send?access_token=e93d3076f9b6a981c15603ecb8328278d0831b9b3b34ca94d6da89105ac65332" ;
 
 }
