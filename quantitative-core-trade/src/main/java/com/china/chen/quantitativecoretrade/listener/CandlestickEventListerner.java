37b6e048daa02405a454bbd80b96bdd522b33cbf
chenjianjun <chenjianjun@huobi.com>
2019-11-28 11:02:42 +0800

    message: add base function

--- quantitative-core-trade/src/main/java/com/china/chen/quantitativecoretrade/listener/CandlestickEventListerner.java
+++ quantitative-core-trade/src/main/java/com/china/chen/quantitativecoretrade/listener/CandlestickEventListerner.java
@@ -1,6 +1,9 @@
 package com.china.chen.quantitativecoretrade.listener;
 
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+
 import com.china.chen.quantitativecoretrade.constants.TradePairEnum;
 import com.china.chen.quantitativecoretrade.utils.DingTalkUtils;
 import com.huobi.client.SubscriptionListener;
@@ -8,17 +11,25 @@ import com.huobi.client.model.Candlestick;
 import com.huobi.client.model.enums.CandlestickInterval;
 import com.huobi.client.model.event.CandlestickEvent;
 
+import org.apache.commons.lang.StringUtils;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class CandlestickEventListerner implements SubscriptionListener<CandlestickEvent> {
+
+
+    private static Cache<String,String> cache = CacheBuilder.newBuilder()
+                  .expireAfterWrite(3,TimeUnit.MINUTES)
+                  .build();
 
     /**每个交易对、不同时间间隔的K线数据容量*/
     private static final Integer CANDLESTICK_CAPACITY = 30 ;
@@ -54,8 +65,12 @@ public class CandlestickEventListerner implements SubscriptionListener<Candlesti
             BigDecimal margin = candlestick.getClose().subtract(candlestick.getOpen()) ;
             BigDecimal ratio = margin.divide(candlestick.getOpen(),3,RoundingMode.HALF_UP) ;
             /**如果低涨幅的绝对值大于2，则证明有行情*/
-            if(ratio.abs().compareTo(new BigDecimal(2+"")) > 0){
-                DingTalkUtils.sendMarkDown("暴涨暴跌告警","币种信息： "+candlestickEvent.getSymbol());
+            if(ratio.abs().compareTo(new BigDecimal("0.02")) > 0){
+
+                if(StringUtils.isEmpty(cache.getIfPresent(candlestickEvent.getSymbol()))){
+                    DingTalkUtils.sendMarkDown("暴涨暴跌告警","币种信息： "+candlestickEvent.getSymbol()+" 幅度："+ratio);
+                    cache.put(candlestickEvent.getSymbol(),ratio.toPlainString());
+                }
             }
         }
     }
